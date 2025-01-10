16cba26807f07a391363a5b1eec7c2d6c68b46e8
Shubham <prajapatishubh9881@gmail.com>
2025-01-10 13:36:17 +0530

    shopping cart product page

---
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.ecom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/index")
+    public String ShowIndex() {
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+    @GetMapping("/base")
+    public String base() {
+        return "base";
+    }
+}
